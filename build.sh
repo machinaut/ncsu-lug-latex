e604febdf84e42d67f485d1aea505ad42a6a9c94
Alex Ray <alex@alex-laptop.(none)>
2009-11-01 19:20:55 -0500

    consolidate scripts

--- build.sh
+++ build.sh
@@ -20,3 +20,15 @@ pdflatex presentation.tex
 pdflatex presentation.tex
 mv presentation.pdf ..
 cd ..
+
+#cleanup script for all the extra generated files
+
+#clean hello
+cd hello
+rm hello.aux hello-code.tex hello.log hello.pdf
+#clean presentation
+cd ../presentation
+rm presentation.aux presentation.log \
+   presentation.nav presentation.out \
+   presentation.snm presentation.toc pygments.aux
+cd ..
