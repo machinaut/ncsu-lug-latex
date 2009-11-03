c66b89dee474206c2c4d6429b92fac27a518c9f8
Alex Ray <alex@alex-laptop.(none)>
2009-11-03 15:07:01 -0500

    hebrew stuff

--- build.sh
+++ build.sh
@@ -32,6 +32,8 @@ cd ..
 #clean hello
 cd hello
 rm hello.aux hello-code.tex hello.log hello.pdf
+cd ../examples
+rm hebrew_ex.aux hebrew_ex.log
 #clean presentation
 cd ../presentation
 rm presentation.aux presentation.log \
